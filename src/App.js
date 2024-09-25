f03b75c98cc7bb9d1e6343e079e47fe645b78159
prachi1211 <prachijethava1211@gmail.com>
2024-09-25 12:06:12 -0400

    default path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
@@ -18,11 +18,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, updateLoad] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      upadateLoad(false);
+      updateLoad(false);
     }, 1200);
 
     return () => clearTimeout(timer);
@@ -35,11 +35,16 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
+          {/* Default route for Home */}
           <Route path="/" element={<Home />} />
+
+          {/* Other routes */}
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+
+          {/* Redirect any undefined route to Home */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
