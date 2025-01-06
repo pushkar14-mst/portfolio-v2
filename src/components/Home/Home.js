ae5b421a67e08613e4bb29509e90fec39fe54170
prachi1211 <prachijethava1211@gmail.com>
2025-01-06 05:19:27 -0500

    photo update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/prachi_formal_final.jpg";
+import homeLogo from "../../Assets/file.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
