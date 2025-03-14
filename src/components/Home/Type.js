9b884e9bd2ff6404c51c9ca0461ba34b35a249bb
prachi1211 <prachijethava1211@gmail.com>
2025-03-14 16:23:12 -0700

    updated home page

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -3,20 +3,29 @@ import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
-    <Typewriter
-      options={{
-        strings: [
-          "Data Strategist",
-          "Software Engineer",
-          "Data Analyst",
-          "Data Scientist",
-          "Software Developer",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
+    <div
+      style={{
+        display: "flex",
+        justifyContent: window.innerWidth < 768 ? "flex-start" : "center",
+        alignItems: "center",
+        paddingLeft: window.innerWidth < 768 ? "20px" : "0px", // Adds left padding for mobile
       }}
-    />
+    >
+      <Typewriter
+        options={{
+          strings: [
+            "Data Strategist",
+            "Software Engineer",
+            "Data Analyst",
+            "Data Scientist",
+            "Software Developer",
+          ],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 50,
+        }}
+      />
+    </div>
   );
 }
 
